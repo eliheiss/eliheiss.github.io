adee369af0924908a51833c713c127907d7f546d
Elijah <e@eliheiss.com>
2014-03-26 19:55:13 -0400

    /m/

---
+++ m/index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<!--
+       _      _
+  ___ (_)__ _| |_    __ ___
+ / -_)| / _` | ' \ _/ _/ _ \
+ \___|/ \__, |_||_(_)__\___/
+    |__/|___/
+
+-->
+<html lang="en" itemscope="" itemtype="http://schema.org/WebPage">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=0">
+<link rel="stylesheet" href="//fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css">
+<link rel="stylesheet" href="//ejgh.co/style.css">
+<script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+<script src="//ejgh.co/script.js"></script>
+<title>ejgh.co</title>
+</head>
+<body>
+	<div id="vp">
+		<div id="sbc">
+			<!--<div class="sbc-wrap">-->
+        <h5>Your Links</h5>
+        <p>
+          <a href="">Home</a>
+          <a href="">News</a>
+          <a href="">Posts</a>
+          <a href="">Mentions</a>
+          <a href="">Profile</a>
+          <a href="">Messages</a>
+          <a href="">Find Friends</a>
+          <a href="">Trending</a>
+          <a href="">Events</a>
+          <a href="">Photos</a>
+          <a href="">Pages</a>
+          <a href="">Friends</a>
+          <a href="">Following</a>
+          <a href="">Followers</a>
+          <a href="">Favorites</a>
+          <a href="">Find Blogs</a>
+          <a href="">Settings</a>
+          <a href="">Lists</a>
+          <a href="">Links</a>
+        </p>
+        <h5>Your Links</h5>
+        <p>
+          <a href="">Home</a>
+          <a href="">News</a>
+          <a href="">Posts</a>
+          <a href="">Mentions</a>
+          <a href="">Profile</a>
+          <a href="">Messages</a>
+          <a href="">Find Friends</a>
+          <a href="">Trending</a>
+          <a href="">Events</a>
+          <a href="">Photos</a>
+          <a href="">Pages</a>
+          <a href="">Friends</a>
+          <a href="">Following</a>
+          <a href="">Followers</a>
+          <a href="">Favorites</a>
+          <a href="">Find Blogs</a>
+          <a href="">Settings</a>
+          <a href="">Lists</a>
+          <a href="">Links</a>
+        </p>
+        <h5>Your Links</h5>
+        <p>
+          <a href="">Home</a>
+          <a href="">News</a>
+          <a href="">Posts</a>
+          <a href="">Mentions</a>
+          <a href="">Profile</a>
+          <a href="">Messages</a>
+          <a href="">Find Friends</a>
+          <a href="">Trending</a>
+          <a href="">Events</a>
+          <a href="">Photos</a>
+          <a href="">Pages</a>
+          <a href="">Friends</a>
+          <a href="">Following</a>
+          <a href="">Followers</a>
+          <a href="">Favorites</a>
+          <a href="">Find Blogs</a>
+          <a href="">Settings</a>
+          <a href="">Lists</a>
+          <a href="">Links</a>
+        </p>
+			<!--</div>--><!-- /.sbc-wrap -->
+		</div><!-- /#sbc -->
+		<div id="ew">
+			<div id="emc">
+				<h1><strong>h1.</strong> This is a test header for H1. Let's see what happens when it is extra long.</h1>
+				<h2><strong>h2.</strong> And here is an H2 header. How long should this headline be in order to make it wrap?</h2>
+				<h3><strong>h3.</strong> Here is an H3 tag with not too much text.</h3>
+				<h4><strong>h4.</strong> An example H4 tag.</h4>
+				<h5><strong>h5.</strong> Here is an example of an H5 tag.</h5>
+				<h6><strong>h6.</strong> And lastly, here is an example of a very long headline for the H6.</h6>
+				<h1>Who I am.</h1>
+				<p>My name is <a href="http://eliheiss.net/">Elijah Heiss</a>, and I am the owner of <strong>ejgh.co</strong>.</p>
+				<p>I build Web sites for a living. My passion is Front End Development - where the concept and code meets the user to create an experience.</p>
+				<p>I strive to always try and improve that experience for every Web site I help build. And with my over 10 years of knowledge and expertise in my field, I have plenty of know how to get the job done for you.</p>
+				<h4>Lorem Ipsum</h4>
+				<p>Ut at sem ut nibh molestie tempus. Nunc volutpat malesuada massa, vel auctor odio consequat consectetur. Suspendisse scelerisque nunc non quam suscipit vestibulum. Aliquam eu turpis sem, non ultrices tellus. Pellentesque vel velit lectus, a imperdiet arcu. Vivamus et turpis in libero consectetur gravida non id massa. Morbi ut urna mi. Phasellus pellentesque congue porttitor.</p>
+				<h4>What is Lorem Ipsum?</h4>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>Where does it come from?</h4>
+				<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+				<h4>Why do we use it?</h4>
+				<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+				<h4>Where can I get some?</h4>
+				<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+			</div><!-- /#emc -->
+			<div id="ehb">
+				<div id="hsr">
+					<div class="_UI"><i class="fa fa-bars"></i>
+					</div><!-- /._UI -->
+					<div class="_SI"><i class="fa fa-user"></i></div>
+				</div><!-- /#hsr -->
+				<div id="hsl">
+					<div class="_EJ"><i class="fa fa-comments-o"></i></div>
+				</div><!-- /#hsl -->
+			</div><!-- /#ehb -->
+			<div id="efb">
+				<div id="fsr">
+					<div class="_TS"><a href="">Privacy &amp; Terms</a></div>
+				</div><!-- /#fsr -->
+				<div id="fsl">
+					<div class="_AB"><a href="">About</a></div>
+				</div><!-- /#fsl -->
+			</div><!-- /#efb -->
+		</div><!-- /#ew -->
+	</div><!-- /#vp -->
+</body>
+</html>
---
+++ m/script.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	/*nav*/
+	$('._UI').click(function() {
+        $('#vp').toggleClass('_ii');
+	});
+  $('._ii ._UI').click(function() {
+      $('#vp').removeClass('_ii');
+  });
+	$('._EJ').click(function() {
+        $('#vp').toggleClass('_ss');
+				if ($('#vp').hasClass('_ss')) {
+					$('#ew').height(
+						$(window).height()
+					);
+				}
+	});
+  $('._ss #ew').click(function() {
+      $('#vp').removeClass('_ss');
+  });/*/nav*/
+
+	/*shrink*/
+	$(window).scroll(function () {
+	  if ($(document).scrollTop() > 1 ) {
+	    $('#ehb,#efb').addClass('_sh');
+	  } else {
+	    $('#ehb,#efb').removeClass('_sh');
+	  }
+	});/*/shrink*/
+
+});/*/$*/
